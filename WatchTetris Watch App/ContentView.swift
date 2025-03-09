27299888f65c68795b81f7f8802f4c3affe2d4d4
MacOmer <justletomer@gmail.com>
2025-03-09 12:32:24 +0900

    Initial Commit

---
+++ WatchTetris Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchTetris Watch App
+//
+//  Created by Omer on 3/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchTetris Watch App/WatchTetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTetrisApp.swift
+//  WatchTetris Watch App
+//
+//  Created by Omer on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchTetris_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
